35a2e3cd0a727243ad4c651674bf29187b332309
DiyorMr <dmuxiddinov594@gmail.com>
2026-05-30 19:54:38 +0500

    axios done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
-  const [pokemon, setPokemon] = useState([]);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/")
-      .then((res) => setPokemon(res.data.results))
-      .catch((err) => console.log(err));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(`Xato: ${err}`);
+        setLoading(false);
+      });
   }, []);
 
+  if (loading) return <h2>Loading ...</h2>;
+  if (error) return <h2>{error}</h2>;
+
   return (
-    <div>
-      {pokemon.map((item, index) => (
-        <div key={index}>
-          <h1>{item.name}</h1>
-          <img style={{width:200}}
-            src={`https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg`}
-            alt={item.name}
-          />
+    <div className="container">
+      <h1 className="title">Users</h1>
+      {data.map((item) => (
+        <div className="card" key={item.id}>
+          <p>{item.name}</p>
+          <p>{item.email}</p>
         </div>
       ))}
     </div>
   );
 };
-
 export default App;
